201ac62a8cd258bc45c63e9bc9a9af1a55157eb9
kartikkaushik3011 <kartikkaushik3011@gmail.com>
2024-12-18 12:43:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Body from './components/Body';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Tours from './components/Tours';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Body />
+      <Tours />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
